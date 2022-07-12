5a784c8f66a57f4a3df42f1410e0d3153ffc06d7
thelunarwolf <navadeepraja22@gmail.com>
2022-07-13 02:29:56 +0530

    Fixed an issue with image

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
     class Config:
         env_file = ".env"
